cee0048888b07aef6df14004d61fdb9e6323dbb8
realaashil <singhalaashil@gmail.com>
2025-03-13 17:18:47 +0530

    feat(App): Add signout Button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:expense_split/pages/dena.dart';
 import 'package:expense_split/pages/lena.dart';
 import 'package:expense_split/pages/settings.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -21,12 +22,17 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Future<void> _signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [IconButton(onPressed: _signOut, icon: Icon(Icons.logout))],
       ),
       bottomNavigationBar: Navigation(
         setPageIndex: setPageIndex,
